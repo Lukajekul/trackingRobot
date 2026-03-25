8886a5ca848fa9c3a269577473fffedf5b93453e
SLEEPWELL <66638927+Lukajekul@users.noreply.github.com>
2026-03-25 09:45:48 +0000

    milti target center calculation

--- arUcoTracking.py
+++ arUcoTracking.py
@@ -15,9 +15,49 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (1280, 720)}))
 picam2.start()
 
+def multitargetCenter(cordinates):
+	X, Y = zip(*cordinates)
+	center = (sum(X)/len(X), sum(Y)/len(Y))
+	return center
+
 def aruco_display(corners, ids, rejected, image):
 	if len(corners) > 0:
 		ids = ids.flatten()
+		if len(corners) == 4:
+			for (markerCorner, markerID) in zip(corners, ids):
+				markerCorners = markerCorner.reshape((4, 2))
+				(topLeft, topRight, bottomRight, bottomLeft) = markerCorners
+
+				topRight = (int(topRight[0]), int(topRight[1]))
+				bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+				bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+				topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+				cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+				cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+				cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+				cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+
+				cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+				cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+				
+
+				relX = cX - 640
+				relY = -(cY - 360)
+
+				cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+
+				cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+				cv2.putText(image, f"({cX}, {cY})", (cX + 10, cY - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+				print(f"Marker ID {markerID} | Center: ({cX}, {cY})")
+		return image
+	
+
+	else: 
+		cordinates = []
 		for (markerCorner, markerID) in zip(corners, ids):
 			markerCorners = markerCorner.reshape((4, 2))
 			(topLeft, topRight, bottomRight, bottomLeft) = markerCorners
@@ -35,6 +75,8 @@ def aruco_display(corners, ids, rejected, image):
 			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
 			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 			
+			relCord = [cX, cY]
+			cordinates.append(relCord)
 
 			relX = cX - 640
 			relY = -(cY - 360)
@@ -48,7 +90,11 @@ def aruco_display(corners, ids, rejected, image):
 
 			print(f"Marker ID {markerID} | Center: ({cX}, {cY})")
 
-	return image
+		center = multitargetCenter(cordinates)
+
+		cv2.circle(image, (center[0], center[1]), 4, (255, 255, 0), -1)
+
+		return image
 
 def get_frame(tracking):
 	if tracking:
